6d1e88e84ea6611dadfdffd6d5c36c07c3cdaf9f
sojeonglee <sojeonglee0125@gmail.com>
2022-10-31 21:17:59 +0900

    feat : @Controller 와 @RequestMapping 을 활용한 핸들러 구현

---
+++ src/main/java/com/spring/servlet/web/springmvc/v1/SpringMemberFormControllerV1.java
@@ -0,0 +1,15 @@
+package com.spring.servlet.web.springmvc.v1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+// @Component + @RequestMapping 로도 작동한다.
+public class SpringMemberFormControllerV1 {
+
+    @RequestMapping("/springmvc/v1/members/new-form")
+    public ModelAndView process(){
+        return new ModelAndView("new-form");
+    }
+}
